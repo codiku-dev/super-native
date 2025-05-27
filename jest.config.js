9625aae33e5dbd22f858e686c28fa8c3debdaa28
Codiku <codiku.dev@gmail.com>
2025-05-27 02:45:04 +0200

    test are working

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   preset: 'react-native',
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-react'] }]
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation|@react-native-community|@rn-primitives)/)',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  setupFiles: ['<rootDir>/jest.setup.js']
 };
