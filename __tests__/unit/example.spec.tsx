e0763d642fd42144968995eb18ecf9d1eeff2aa4
Codiku <codiku.dev@gmail.com>
2025-05-27 02:48:06 +0200

    feat(test-setup) : unit test

--- __tests__/unit/example.spec.tsx
+++ __tests__/unit/example.spec.tsx
@@ -1,14 +1,7 @@
 import { render } from '@testing-library/react-native';
-import { Button } from 'react-native';
-
 import { Text } from '@/components/ui/text';
 
-describe('Button Component', () => {
-  it('renders correctly a native button', () => {
-    const { getByRole } = render(<Button title='Test Button' />);
-    expect(getByRole('button')).toBeTruthy();
-  });
-
+describe('Test the text component from RNR package', () => {
   it('renders correctly a text (rnr)', () => {
     const { getByText } = render(<Text>Test Text</Text>);
     expect(getByText('Test Text')).toBeTruthy();
